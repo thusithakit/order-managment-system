2cdf67dc6946ee516f8faed9eeb7f53f708e505a
Thusithakit <thusithakit3@gmail.com>
2025-07-23 00:38:34 -0700

    added view order status

--- app/(customer)/search-order/[id]/page.tsx
+++ app/(customer)/search-order/[id]/page.tsx
@@ -1,14 +1,20 @@
-const fetchOrder = async () => {
+import axios from "axios";
+
+const fetchOrder = async (id: number) => {
   try {
+    const response = await axios.get(`http://localhost:3001/orders/${id}`);
+    return response.data;
   } catch (err) {
     console.log(err);
   }
 };
 const page = async ({ params }: { params: Promise<{ id: number }> }) => {
   const { id } = await params;
+  const orderDetails = await fetchOrder(id);
   return (
     <div>
       <h1>View Order status of order number {id}</h1>
+      <p>Progress of the order: {orderDetails.progress}</p>
     </div>
   );
 };
